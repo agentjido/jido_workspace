2e695cf38fae4471f7b304f43d34c052d512bf71
Mike Hostetler <84222+mikehostetler@users.noreply.github.com>
2026-04-10 14:38:33 -0500

    ci: roll out shared github-actions workflows

--- config/config.exs
+++ config/config.exs
@@ -4,5 +4,35 @@ import Config
 config :jido_workspace,
   default_adapter: Jido.VFS.Adapter.InMemory
 
+if config_env() == :dev do
+  config :git_hooks,
+    auto_install: true,
+    verbose: true,
+    hooks: [
+      commit_msg: [
+        tasks: [
+          {:cmd, "mix git_ops.check_message", include_hook_args: true}
+        ]
+      ]
+    ]
+
+  config :git_ops,
+    mix_project: Jido.Workspace.MixProject,
+    changelog_file: "CHANGELOG.md",
+    repository_url: "https://github.com/agentjido/jido_workspace",
+    manage_mix_version?: true,
+    version_tag_prefix: "v",
+    types: [
+      feat: [header: "Features"],
+      fix: [header: "Bug Fixes"],
+      perf: [header: "Performance"],
+      refactor: [header: "Refactoring"],
+      docs: [hidden?: true],
+      test: [hidden?: true],
+      chore: [hidden?: true],
+      ci: [hidden?: true]
+    ]
+end
+
 # Import environment specific config
 import_config "#{config_env()}.exs"
